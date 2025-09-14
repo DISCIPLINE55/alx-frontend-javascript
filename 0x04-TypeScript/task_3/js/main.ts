9bd99570cf41cc1778f89c4f7f641287605f11e4
DISCIPLINE55 <ismailmensah055@gmail.com>
2025-09-14 12:25:56 +0000

    tasks error resolved in task_3

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,14 +1,12 @@
-/// <reference path="./crud.d.ts" />
-
+import { insertRow, updateRow, deleteRow } from './crud';
 import { RowID, RowElement } from './interface';
-import * as CRUD from './crud';
 
 const row: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva'
+ firstName: 'Guillaume',
+ lastName: 'Salva'
 };
 
-const newRowID: RowID = CRUD.insertRow(row);
+const newRowID: RowID = insertRow(row);
 const updatedRow: RowElement = { ...row, age: 23 };
-CRUD.updateRow(newRowID, updatedRow);
-CRUD.deleteRow(newRowID);
+updateRow(newRowID, updatedRow);
+deleteRow(newRowID);
